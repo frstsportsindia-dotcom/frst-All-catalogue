3a54ba3d9c0054461d1b72d641b6738a373e2580
frstsportsindia-dotcom <frstsportsindia@gmail.com>
2026-09-16 13:07:57 +0530

    Add files via upload

---
+++ designs.js
@@ -0,0 +1,52 @@
+window.FRST_DESIGNS = [
+  {
+    "code": "CR-001",
+    "sport": "Cricket",
+    "image": "images/cr-001.webp"
+  },
+  {
+    "code": "CR-002",
+    "sport": "Cricket",
+    "image": "images/cr-002.webp"
+  },
+  {
+    "code": "CR-003",
+    "sport": "Cricket",
+    "image": "images/cr-003.webp"
+  },
+  {
+    "code": "CR-004",
+    "sport": "Cricket",
+    "image": "images/cr-004.webp"
+  },
+  {
+    "code": "CR-005",
+    "sport": "Cricket",
+    "image": "images/cr-005.webp"
+  },
+  {
+    "code": "CR-006",
+    "sport": "Cricket",
+    "image": "images/cr-006.webp"
+  },
+  {
+    "code": "CR-007",
+    "sport": "Cricket",
+    "image": "images/cr-007.webp"
+  },
+  {
+    "code": "CR-008",
+    "sport": "Cricket",
+    "image": "images/cr-008.webp"
+  },
+  {
+    "code": "CR-009",
+    "sport": "Cricket",
+    "image": "images/cr-009.webp"
+  },
+  {
+    "code": "CR-010",
+    "sport": "Cricket",
+    "image": "images/cr-010.webp"
+  }
+];
